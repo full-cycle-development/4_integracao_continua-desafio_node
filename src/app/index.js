e5db6d5e22681590e733d2eb49be37317c9f2540
Wesley Xavier <wesley@trcx.com.br>
2024-01-03 12:39:36 -0300

    feat: :hammer: adicionar script development

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get("/", async (request, response) => {
-  return response.status(200).json({ status: 200, message: "Full Cycle Rocks!", date: new Date() });
+  return response.status(200).json({ status: 200, message: "Full Cycle Rocks!" });
 });
 
 module.exports = { app }
